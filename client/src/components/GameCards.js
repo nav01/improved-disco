d56688793a96d6b62c836e52823c2284a067af87
Nav <nav01@mail.fresnostate.edu>
2019-07-09 14:26:01 -0700

    Added working page numbers, game information loads based on current page.

--- client/src/components/GameCards.js
+++ client/src/components/GameCards.js
@@ -5,19 +5,61 @@ import {games} from '../data';
 import './game-cards.css';
 
 class GameCards extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.gamesPerPage = 40; //changes if visiting on mobile, revisit.
+    this.numPages = Math.ceil(games.length / this.gamesPerPage);
     this.games = games.slice(0,20);
+
+    this.state = {
+      currentPage: 1,
+    }
   }
 
+  incrementPage  = () => {
+    this.setState(prevState => {
+      let page = prevState.currentPage + 1;
+      return {currentPage: (page > this.numPages ? 1 : page)};
+    });
+  }
+
+  decrementPage = () => {
+    this.setState(prevState => {
+      let page = prevState.currentPage - 1;
+      return {currentPage: (page <= 0 ? this.numPages : page)};
+    });
+  }
+
+  changePage = (pageNum) => {
+    this.setState(prevState =>
+      prevState.currentPage === pageNum ? {}
+      : {currentPage: pageNum}
+    );
+  }
 
   render() {
+    var gamesIndexStart = (this.state.currentPage - 1) * this.gamesPerPage;
+    var gamesIndexEnd = gamesIndexStart + this.gamesPerPage > games.length ? games.length : gamesIndexStart + this.gamesPerPage;
+
+    console.log(gamesIndexStart);
+    console.log(gamesIndexEnd);
     return (
       <div id="game-cards">
-        {this.games.map((game) =>
-          <GameCard gameName={game.title} image={game.image} exclusive={game.moreDetails.exclusive} visible={true} />
+        {games.slice(gamesIndexStart, gamesIndexEnd).map((game) =>
+          <GameCard
+            gameName={game.title}
+            image={game.image} exclusive={game.moreDetails.exclusive}
+            visible={true}
+          />
         )}
-        <GamesNav />
+        <GamesNav
+          numPages={this.numPages}
+          currentPage={this.state.currentPage}
+          changePage={this.changePage}
+          incrementPage={this.incrementPage}
+          decrementPage={this.decrementPage}
+        />
       </div>
     );
   }
--- client/src/components/GamesNav.js
+++ client/src/components/GamesNav.js
@@ -6,55 +6,30 @@ import chevronRight from '../icons/chevron-right.svg';
 class GamesNav extends React.Component {
   constructor (props) {
     super(props);
-
-    this.pages = 7;
-    this.state = {
-      page: 1,
-    }
   }
-
-  changePage = (pageNum) => {
-    this.setState((prevState) =>
-      prevState.page === pageNum ? {}
-      : {page: pageNum}
-    );
-  }
-
-  incrementPage = () => {
-    this.setState((prevState) => ({
-      page: (prevState.page + 1) % this.pages,
-    }));
-  }
-
-  decrementPage = () => {
-    this.setState((prevState) => ({
-      page: ((prevState.page - 1 + this.pages) % this.pages),
-    }));
-  }
-
 
   render () {
-    var pages = [...Array(7).keys()];
+    var pages = Array.from({length: this.props.numPages}, (x,i) => i + 1);
 
     return (
       <div id="announcements-nav">
-        <a id="nav-left" href="#" onClick={this.decrementPage}>
+        <a id="nav-left" href="#" onClick={this.props.decrementPage}>
           <img className="nav-chevron" src={chevronLeft} />
         </a>
         {
           pages.map((page) => {
             return (
               <a
-                className={this.state.page === page ? "nav-page-num nav-page-num-selected" : "nav-page-num"}
+                className={this.props.currentPage === page ? "nav-page-num nav-page-num-selected" : "nav-page-num"}
                 href="#"
-                onClick={() => this.changePage(page)}
-                >
+                onClick={() => this.props.changePage(page)}
+              >
               {page}
               </a>
             )
           })
         }
-        <a id="nav-right" href="#" onClick={this.incrementPage}>
+        <a id="nav-right" href="#" onClick={this.props.incrementPage}>
           <img className="nav-chevron" src={chevronRight} />
         </a>
       </div>
